02f6abf9f330c25c3ded1d11bdaa197d8bdab2e5
matthias <h1395010@connect.hku.hk>
2015-04-23 14:09:49 +0800

    people.txt

--- src/main/java/Q/Wikidata_Q_Reader.java
+++ src/main/java/Q/Wikidata_Q_Reader.java
@@ -76,15 +76,14 @@ public class Wikidata_Q_Reader
     // add Q values to their arrayList in the hash map at the index of the appropriate entity
     public static HashSet<String> put_to_hash(String key, String value) 
     {
-        if (!q_valMap.containsKey(key)) 
-        {
-            return q_valMap.put(key, new HashSet<String>() );
-        }
-        HashSet<String> list = q_valMap.get(key);
-        list.add(value);
-        return q_valMap.put(key, list);
+        HashSet<String> valSet;
+        if (q_valMap.containsKey(key)) {
+            valSet = q_valMap.get(key);
+        } else {
+            valSet = new HashSet<String>();
+            q_valMap.put(key, valSet);
+        } 
+        valSet.add(value);
+        return valSet;
     }
 }
-
-
-
