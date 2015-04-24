65176bbcce9d5b975317c1ab40c083303b14b899
matthias <h1395010@connect.hku.hk>
2015-04-24 12:28:27 +0800

    seems to be working

--- src/main/java/Q/Wikidata_Q_Reader.java
+++ src/main/java/Q/Wikidata_Q_Reader.java
@@ -19,6 +19,7 @@ import java.util.regex.Pattern;
 import org.apache.commons.validator.routines.UrlValidator;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 public class Wikidata_Q_Reader 
 {
@@ -30,74 +31,61 @@ public class Wikidata_Q_Reader
     {
 
 
-    	//get the corresponding wikidata page
-    	//check the validity of the URL
-    	String URL_czech = "https://www.wikidata.org/wiki/Special:ItemByTitle?site=en&page=" + associated_alias + "&submit=Search";
-    	URL wikidata_page = new URL(URL_czech);
-    	HttpURLConnection wiki_connection = (HttpURLConnection)wikidata_page.openConnection();
-    	InputStream wikiInputStream = null;
-    	
-    	
-    	try 
-    	{
-		    // try to connect and use the input stream
-		    wiki_connection.connect();
-		    wikiInputStream = wiki_connection.getInputStream();
-    	} 
-    	catch(IOException e) 
-    	{
-		    // failed, try using the error stream
-		    wikiInputStream = wiki_connection.getErrorStream();
-    	}
-    	
-        
-
-	        BufferedReader wiki_data_pagecontent = new BufferedReader(
-	                                			   	   new InputStreamReader(
-	                                			   			wikiInputStream ));
-	        String line_by_line;
-	        
-	        
-	
-	        while ((line_by_line = wiki_data_pagecontent.readLine()) != null) 
-	        {
-	        	// if we can determine it's a disambig page we need to send it off to get all 
-	        	// the possible senses in which it can be used.
-	        	Pattern disambig_pattern = Pattern.compile("<div class=\"wikibase-entitytermsview-heading-description \">Wikipedia disambiguation page</div>");
-	        	Matcher disambig_indicator = disambig_pattern.matcher(line_by_line);
-	            if (disambig_indicator.matches()) 
-	            {
-	            	//off to get the different usages
-	            	Wikipedia_Disambig_Fetcher.all_possibilities(  platonic_key, associated_alias );
-	            }
-	            else
-	            {
-		        	//get the Q value off the page by matching
-		        	Pattern q_page_pattern = Pattern.compile("<!-- wikibase-toolbar --><span class=\"wikibase-toolbar-container\"><span class=\"wikibase-toolbar-item " +
-		                    "wikibase-toolbar \">\\[<span class=\"wikibase-toolbar-item wikibase-toolbar-button wikibase-toolbar-button-edit\"><a " +
-		                    "href=\"/wiki/Special:SetSiteLink/(.*?)\">edit</a></span>\\]</span></span>");
+		//get it's normal wiki disambig page
+		String URL_czech = "https://www.wikidata.org/wiki/Special:ItemByTitle?site=en&page=" + associated_alias + "&submit=Search";
+		URL wikidata_page = new URL(URL_czech);
+		HttpURLConnection wiki_connection = (HttpURLConnection)wikidata_page.openConnection();
+		InputStream wikiInputStream = null;
 		
-		            Matcher match_Q_component = q_page_pattern.matcher(line_by_line);
-		            if ( match_Q_component.matches() ) 
-		            {
-		                String Q = match_Q_component.group(1);
-		                
-		                // 'Q' should be appended to an array, since each entity can hold multiple
-		                // Q values on that basis of disambig
-		                put_to_hash( platonic_key, Q );
-		            }
-	            }
-	
-	        }
-	        wiki_data_pagecontent.close();
+
+			try 
+			{
+			    // try to connect and use the input stream
+			    wiki_connection.connect();
+			    wikiInputStream = wiki_connection.getInputStream();
+			} 
+			catch(IOException e) 
+			{
+			    // failed, try using the error stream
+			    wikiInputStream = wiki_connection.getErrorStream();
+			}
+			// parse the input stream using Jsoup
+			Document docx = Jsoup.parse(wikiInputStream, null, wikidata_page.getProtocol()+"://"+wikidata_page.getHost()+"/");
+			
+
+        	// if we can determine it's a disambig page we need to send it off to get all 
+        	// the possible senses in which it can be used.
+			String disambig = docx.select("div.wikibase-entitytermsview-heading-description").text(); 
+			String qidmazib = "Wikipedia disambiguation page";
+
+			
+			
+            if ( !disambig.toLowerCase().contains(qidmazib.toLowerCase()) ) 
+            {
+    			String Q = docx.select("span.wikibase-entitytermsview-heading-label-id").text().replaceAll("[()]", "");
+			                
+                // 'Q' should be appended to an array, since each entity can hold multiple
+                // Q values on that basis of disambig
+                put_to_hash( platonic_key, Q );
+
+            }
+            else if ( !Wikipedia_Disambig_Fetcher.urlsVisited.contains(platonic_key + associated_alias) )
+            {
+            	//off to get the different usages
+            	Wikipedia_Disambig_Fetcher.all_possibilities(  platonic_key, associated_alias );
+            }
+		
+	        
+	        //Wikipedia_Disambig_Fetcher.urlsVisited.add(wikidata_page);
 
 	        // \\ // ! PRINT IT ! // \\ // \\ // \\ // \\ // \\ // \\
 	        for (Map.Entry<String, HashSet<String> > entry : q_valMap.entrySet()) 
 	        {
 	            System.out.println(entry.getKey()+" : " + Arrays.deepToString(q_valMap.entrySet().toArray()) );
 	        }
-
 	        
+    	
+    
    
     }
     
--- src/main/java/Q/Wikipedia_Disambig_Fetcher.java
+++ src/main/java/Q/Wikipedia_Disambig_Fetcher.java
@@ -7,6 +7,7 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.validator.routines.UrlValidator;
 import org.jsoup.Jsoup;
@@ -16,8 +17,14 @@ import org.jsoup.select.Elements;
 
 public class Wikipedia_Disambig_Fetcher 
 {
+	static Set<String> urlsVisited = new HashSet<String>();
+	static Set<String> linqVisited = new HashSet<String>();
+	
     public static void all_possibilities( String platonic_key, String associated_alias ) throws Exception
     {
+
+    	urlsVisited.add(platonic_key + associated_alias);
+    	
 		System.out.println("this is a disambig page");
 		//if it's a disambig page we know we can go right to the wikipedia
 
@@ -28,7 +35,7 @@ public class Wikipedia_Disambig_Fetcher
 		HttpURLConnection wiki_connection = (HttpURLConnection)wikidata_page.openConnection();
 		InputStream wikiInputStream = null;
 		
-
+	        
 		try 
 		{
 		    // try to connect and use the input stream
@@ -46,21 +53,35 @@ public class Wikipedia_Disambig_Fetcher
 		
         //this can handle the less structured ones. 
         Elements linx = docx.select( "p:contains(" + associated_alias + ") ~ ul a:eq(0)" );
+            
+        System.out.println("This is linx");
+        System.out.println(linx.text());
         
         for (Element linq : linx) 
         {
         	System.out.println(linq.text());
         	
+        	
         	String linq_nospace = URLEncoder.encode( linq.text() , "UTF-8");
-        	Wikidata_Q_Reader.getQ( platonic_key, linq_nospace );
-
+        	
+        	//make sure we're still on track
+        	if ( linq_nospace.toLowerCase().contains(platonic_key.toLowerCase()) ) 
+        	{
+        		Wikidata_Q_Reader.getQ( platonic_key, linq_nospace );
+        	}
+        	
+        	urlsVisited.add(platonic_key + linq_nospace);
     	}
+ 
 
 	    
 
-
+        
     }
     
+ 
+
+
 
 }
 
