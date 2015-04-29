c5b2aec1608a6cc8935095f718c3bd31c03b38c4
matthias <h1395010@connect.hku.hk>
2015-04-29 14:02:59 +0800

    mostly works

--- src/main/java/Q/Label_Assigner.java
+++ src/main/java/Q/Label_Assigner.java
@@ -115,8 +115,10 @@ public class Label_Assigner
 	            for (Element sentence : sentences) 
 		        {
 		        	//hold the q values we need to search over
-	            	List<String> Q_value_references_for_sentence_entities = new ArrayList<String>();
-	            	List<String> English_names_references_for_sentence_entities = new ArrayList<String>();
+	            	//List<String> Q_value_references_for_sentence_entities = new ArrayList<String>();
+	            	//List<String> English_names_references_for_sentence_entities = new ArrayList<String>();
+	            	
+	            	Map<String,String> Q_value_references_for_sentence_entities__and_ENGLISH_NAME = new HashMap<String,String>();
 		        	
 	            	//store the sentence number
 	            	sentence_number = sentence.select("sentence").text();
@@ -157,8 +159,10 @@ public class Label_Assigner
 			        			
 			        			for (String Q_value : DB__key_to_value_item.getValue()) 
 			        			{
-			        				Q_value_references_for_sentence_entities.add( Q_value );
-			        				English_names_references_for_sentence_entities.add(DB__key_to_value_item.getKey());
+			        				//Q_value_references_for_sentence_entities.add( Q_value );
+			        				//English_names_references_for_sentence_entities.add(DB__key_to_value_item.getKey());
+			        				
+			        				Q_value_references_for_sentence_entities__and_ENGLISH_NAME.put( Q_value, DB__key_to_value_item.getKey());
 			        			}
 			        		}
 		        		}
@@ -167,70 +171,66 @@ public class Label_Assigner
 				        		    
 				        		    
                     //czeher
-		        	//HashSet<String> list_of_relation_URLs = new HashSet<String>();
-		        	
-		        	
-		        	Map<String, ArrayList<String>> URL_and_entities = new HashMap<String, ArrayList<String>>();
-		        	
-
-		        	
-
-				    for (String entity_1 : Q_value_references_for_sentence_entities) 
+					Map<String, Pair<String,String>> URL_and_entities = new HashMap<String, Pair<String,String>>();
+				
+				
+				    for (Entry<String,String> entity_1: Q_value_references_for_sentence_entities__and_ENGLISH_NAME.entrySet()) 
 				    {
 				      
-				      for (String entity_2 : Q_value_references_for_sentence_entities) 
+				      for (Entry<String,String> entity_2: Q_value_references_for_sentence_entities__and_ENGLISH_NAME.entrySet())  
 				      {
-				    		ArrayList<String> entities = new ArrayList<String>();
-				    	    
-				     		String URL_czech = "http://milenio.dcc.uchile.cl/sparql?default-graph-uri=&query=PREFIX+%3A+%3Chttp%3A%2F%2Fwww.wikidata.org%2Fentity%2F%3E%0D%0ASELECT+*+WHERE+%7B%0D%0A+++%3A" 
-				     					       + entity_1 + "+%3FsimpleProperty+%3A" 
-				     					       + entity_2 + "%0D%0A%7D%0D%0A&format=text%2Fhtml&timeout=0&debug=on";
-				     		             		
-				    		URL wikidata_page = new URL(URL_czech);
-				    		HttpURLConnection wiki_connection = (HttpURLConnection)wikidata_page.openConnection();
-				    		InputStream wikiInputStream = null;
-				    		
-							try 
-							{
-							    // try to connect and use the input stream
-							    wiki_connection.connect();
-							    wikiInputStream = wiki_connection.getInputStream();
-							} 
-							catch(IOException error) 
-							{
-							    // failed, try using the error stream
-							    wikiInputStream = wiki_connection.getErrorStream();
-							}
-							// parse the input stream using Jsoup
-							Document docx = Jsoup.parse(wikiInputStream, null, wikidata_page.getProtocol()+"://"+wikidata_page.getHost()+"/");
+				    	    if( entity_1.getValue().toLowerCase().contains( entity_2.getValue().toLowerCase() ) )
+				    	    {
+				    	    	continue;
+				    	    }
+				    	    else
+				    	    {
+					     		String URL_czech = "http://milenio.dcc.uchile.cl/sparql?default-graph-uri=&query=PREFIX+%3A+%3Chttp%3A%2F%2Fwww.wikidata.org%2Fentity%2F%3E%0D%0ASELECT+*+WHERE+%7B%0D%0A+++%3A" 
+					     					       + entity_1.getKey() + "+%3FsimpleProperty+%3A" 
+					     					       + entity_2.getKey() + "%0D%0A%7D%0D%0A&format=text%2Fhtml&timeout=0&debug=on";
+					     		             		
+					    		URL wikidata_page = new URL(URL_czech);
+					    		HttpURLConnection wiki_connection = (HttpURLConnection)wikidata_page.openConnection();
+					    		InputStream wikiInputStream = null;
+					    		
+								try 
+								{
+								    // try to connect and use the input stream
+								    wiki_connection.connect();
+								    wikiInputStream = wiki_connection.getInputStream();
+								} 
+								catch(IOException error) 
+								{
+								    // failed, try using the error stream
+								    wikiInputStream = wiki_connection.getErrorStream();
+								}
+								// parse the input stream using Jsoup
+								Document docx = Jsoup.parse(wikiInputStream, null, wikidata_page.getProtocol()+"://"+wikidata_page.getHost()+"/");
+					
+								
+								Elements link_text = docx.select("table.sparql > tbody > tr:nth-child(2) > td > a");
+								//link_text.text();
+								for (Element l : link_text) 
+							    {
+									String output = l.text();
+									output = output.substring(0, output.length()-1);
+								
+									
+							       
+							       
+							       URL_and_entities.put(output, Pair.of(entity_1.getValue(), entity_2.getValue()));
 				
-							
-							Elements link_text = docx.select("table.sparql > tbody > tr:nth-child(2) > td > a");
-							//link_text.text();
-							for (Element l : link_text) 
-						    {
-								String output = l.text();
-								output = output.substring(0, output.length()-1);
-							
-								
-								
-						       entities.add( entity_1 );
-						       entities.add( entity_2 );
-						       
-						       URL_and_entities.put(output, entities);
-								
-								
-						    }
+									
+									
+							    }
+				    	    }
 				        }		    
 				    }
-                    
+        
                     //now we're back at the sentence level
 					//iter hash
-				    for (Entry<String, ArrayList<String>> url : URL_and_entities.entrySet()) 
+				    for (Entry<String, Pair<String, String>> url : URL_and_entities.entrySet()) 
 				    { 
-				    	//System.out.println( url );
-				
-				    	//go to relation url
 				    	
 				 		
 				 		String P_value = url.getKey().substring(url.getKey().lastIndexOf('P'));
@@ -239,16 +239,16 @@ public class Label_Assigner
 				 		System.out.println( "sentence_number: " + sentence_number );
 				 		
 				 		String value = (String) map.get("properties").get( P_value );
-				 		//System.out.println( "property_value: " + value );
 				 		
 
-				 		
-				 		ArrayList<String> retrieved_entities =  URL_and_entities.get(url);
-						
-						String first__english_lang_Q = retrieved_entities.get(0);
-						String second_english_lang_Q = retrieved_entities.get(1);
+				       
+				       
+						String first__english_lang_Q = URL_and_entities.get( url.getKey() ).getLeft();
+						String second_english_lang_Q = URL_and_entities.get( url.getKey() ).getRight();
 						
 						System.out.println("`(" + value + ")'" + "`( " + first__english_lang_Q + ", " + second_english_lang_Q + ")'");
+						
+						
 
 				 		
 				    }
